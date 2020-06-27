091f1cceea759ed1a7e389d58bd2d5f673f087c0
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-06-28 00:00:12 +0200

    Add description slideshow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import "./styles/style.scss";
 
-import jQuery from "jquery";
+import $ from "jquery";
 import "bootstrap";
 import "popper.js";
 // import "./scripts/skewed";
-import "./scripts/slider";
+// import "./scripts/slider";
+
+import Swiper from 'swiper';
+
+$(document).ready(function() {
+  new Swiper('.swiper-container-descriptions', {
+    initialSlide: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+  });
+
+  new Swiper('.swiper-scroll-container', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    freeMode: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    mousewheel: true,
+  })
+});
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
-import 'slick-carousel';
-import TweenMax from "gsap";
+import 'slick-carousel/slick/slick';
+import {TweenMax, Sine, Quad} from "gsap";
 
 $(document).ready(function() {
 
@@ -8,37 +8,38 @@ $(document).ready(function() {
 
   const $slickTrack = $(".slick-track");
   const $slickCurrent = $(".slick-current");
+  const $slickActive = $(".slick-active");
 
   const slideDuration = 900;
 
   //RESET ANIMATIONS
   // On init slide change
   $slider.on("init", function (slick) {
-    TweenMax.to($(".slick-track"), 0.9, {
+    TweenMax.to($slickTrack, 0.9, {
       marginLeft: 0,
     });
-    TweenMax.to($(".slick-active"), 0.9, {
+    TweenMax.to($slickActive, 0.9, {
       x: 0,
       zIndex: 2,
     });
   });
   // On before slide change
   $slider.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-    TweenMax.to($(".slick-track"), 0.9, {
+    TweenMax.to($slickTrack, 0.9, {
       marginLeft: 0,
     });
-    TweenMax.to($(".slick-active"), 0.9, {
+    TweenMax.to($slickActive, 0.9, {
       x: 0,
     });
   });
 
   // On after slide change
   $slider.on("afterChange", function (event, slick, currentSlide) {
-    TweenMax.to($(".slick-track"), 0.9, {
+    TweenMax.to($slickTrack, 0.9, {
       marginLeft: 0,
     });
     $(".slick-slide").css("z-index", "1");
-    TweenMax.to($(".slick-active"), 0.9, {
+    TweenMax.to($slickActive, 0.9, {
       x: 0,
       zIndex: 2,
     });
@@ -46,31 +47,33 @@ $(document).ready(function() {
 
   //SLICK INIT
   $slider.slick({
+    initialSlide: 1,
     speed: slideDuration,
     dots: true,
     waitForAnimate: true,
     useTransform: true,
     cssEase: "cubic-bezier(0.455, 0.030, 0.130, 1.000)",
+    infinite: false
   });
 
   //PREV
   $(".slick-prev").on("mouseenter", function () {
-    TweenMax.to($(".slick-track"), 0.6, {
+    TweenMax.to($slickTrack, 0.6, {
       marginLeft: "180px",
       ease: Quad.easeOut,
     });
-    TweenMax.to($(".slick-current"), 0.6, {
+    TweenMax.to($slickCurrent, 0.6, {
       x: -100,
       ease: Quad.easeOut,
     });
   });
 
   $(".slick-prev").on("mouseleave", function () {
-    TweenMax.to($(".slick-track"), 0.4, {
+    TweenMax.to($slickTrack, 0.4, {
       marginLeft: 0,
       ease: Sine.easeInOut,
     });
-    TweenMax.to($(".slick-current"), 0.4, {
+    TweenMax.to($slickCurrent, 0.4, {
       x: 0,
       ease: Sine.easeInOut,
     });
@@ -78,22 +81,22 @@ $(document).ready(function() {
 
   //NEXT
   $(".slick-next").on("mouseenter", function () {
-    TweenMax.to($(".slick-track"), 0.6, {
+    TweenMax.to($slickTrack, 0.6, {
       marginLeft: "-180px",
       ease: Quad.easeOut,
     });
-    TweenMax.to($(".slick-current"), 0.6, {
+    TweenMax.to($slickCurrent, 0.6, {
       x: 100,
       ease: Quad.easeOut,
     });
   });
 
   $(".slick-next").on("mouseleave", function () {
-    TweenMax.to($(".slick-track"), 0.4, {
+    TweenMax.to($slickTrack, 0.4, {
       marginLeft: 0,
       ease: Quad.easeInOut,
     });
-    TweenMax.to($(".slick-current"), 0.4, {
+    TweenMax.to($slickCurrent, 0.4, {
       x: 0,
       ease: Quad.easeInOut,
     });
