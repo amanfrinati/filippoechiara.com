e73742bc4818d79be4c7ac856c57250e8e3fd8d7
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-07-02 10:33:43 +0200

    Add parallax effect and jquery countdown

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import "./styles/style.scss";
 window.jQuery = require('jquery');
 window.$ = window.jQuery;
 require("jquery-visible");
+require("jquery.countdown");
 
 import "bootstrap";
 import "popper.js";
 // import "./scripts/skewed";
 // import "./scripts/slider";
-
 import Swiper from 'swiper';
+import Rellax from "rellax";
+
 $(document).ready(function() {
   new Swiper('.swiper-container-descriptions', {
     initialSlide: 2,
@@ -30,7 +32,18 @@ $(document).ready(function() {
       el: '.swiper-scrollbar',
     },
     mousewheel: true,
-  })
+  });
+
+  new Rellax(".rellax");
+
+  $('#countdown').countDown('2020/10/10', function(event) {
+    $(this).html(event.strftime(''
+      + '<span>%w</span> weeks '
+      + '<span>%d</span> days '
+      + '<span>%H</span> hr '
+      + '<span>%M</span> min '
+      + '<span>%S</span> sec'));
+  });
 });
 
 $(window).scroll(function(event) {
