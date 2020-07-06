b0795e4467da5c3f41a88a3be5e7b8168a2a73ec
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-07-07 01:31:35 +0200

    Improve webpack configurations

--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,70 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // filename: '[name].[chunkhash].js'
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+        test: /\.(sa|sc|c)ss$/,
+        include: [
+          path.resolve(__dirname, 'src', 'styles')
+        ],
+        use: [
+          {
+            // After all CSS loaders we use plugin to do his work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader",
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader"
+          },
+          {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: "file-loader",
+            options: {
+              outputPath: 'fonts'
+            }
+          }
+        ]
       },
       // {
       //   test: /\.(png|jpe?g|gif)$/i,
@@ -28,24 +73,48 @@ module.exports = {
       //     outputPath: 'assets/images',
       //   },
       // },
+      {
+        // Now we apply rule for images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            // Using file-loader for these files
+            loader: "file-loader",
+
+            // In options we can set different things like format
+            // and directory to save
+            options: {
+              outputPath: 'images'
+            }
+          }
+        ]
+      }
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // filename: 'style.[contenthash].css',
+      filename: 'bundle.css',
+    }),
     new CopyPlugin({
       patterns: [
-        { from: "src/assets/images", to: "assets/images" },
-        // { from: "src/assets/icons", to: "icons" },
-        // { from: "src/assets/site.webmanifest", to: "site.webmanifest" },
-        {
-          from: "src/index.html",
-          to: "index.html",
-        },
+        { from: "src/assets/images", to: "images" },
+    //     // { from: "src/assets/icons", to: "icons" },
+    //     // { from: "src/assets/site.webmanifest", to: "site.webmanifest" },
+    //     {
+    //       from: "src/index.html",
+    //       to: "index.html",
+    //     },
       ],
     }),
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    // new WebpackMd5Hash()
+    require('autoprefixer')
+  ]
 };
