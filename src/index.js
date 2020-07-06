583111b08834ae3ac13ec5732226917a99ca7f41
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-07-07 01:32:30 +0200

    Add Cerimonia section with image

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ window.jQuery = require('jquery');
 window.$ = window.jQuery;
 require("jquery-visible");
 
+import gasp from "gsap"; // Also works with TweenLite and TimelineLite
+import ScrollTrigger from "gsap/dist/ScrollTrigger.min"
 import "bootstrap";
 import "popper.js";
 // import "./scripts/skewed";
@@ -37,6 +39,34 @@ $(document).ready(function() {
   // new Rellax(".rellax", {
   //   center: true
   // });
+
+  gasp.registerPlugin(ScrollTrigger);
+  gasp.to('.eremitani-img',
+  {
+    scale: 1,
+    duration: 1,
+    ease: 'expo.out',
+    scrollTrigger: {
+      trigger: '.hero-cerimonia',
+      start: 'top center',
+      end: "bottom center",
+      // markers: true,
+      scrub: true
+    },
+  });
+
+  gasp.to('.border-wrapper [class*="border-"]',
+  {
+    scaleX: 0,
+    duration: 1,
+    ease: 'expo.out',
+    scrollTrigger: {
+      trigger: '.hero-cerimonia',
+      start: 'top center',
+      end: "bottom center",
+      scrub: true
+    },
+  });
 });
 
 $(window).scroll(function(event) {
