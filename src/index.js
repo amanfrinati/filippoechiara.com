ea253c230bade3d54ec93de5c3eec6a2d2c77a94
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-07-22 02:38:27 +0200

    Remove video.js and typewriter-effect

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ require("jquery-visible");
 
 import "bootstrap";
 import "popper.js";
-import "video.js/dist/video.min";
 import Swiper from 'swiper';
-import Typewriter from 'typewriter-effect/dist/core';
 
 // import "./scripts/skewed";
 // import "./scripts/slider";
@@ -49,36 +47,4 @@ $(document).ready(function() {
     },
     mousewheel: true,
   });
-});
-
-// $(window).scroll(function(event) {
-//   if ($('.quotes p').visible(true)) {
-//     $('.quotes p').addClass("fade-in");
-//   }
-// });
-
-let saveTheDateTyped = false;
-$(window).scroll(function(event) {
-  if ($('#save-the-date').visible(true) && !saveTheDateTyped) {
-    saveTheDateTyped = true;
-
-    const typewriter = new Typewriter(document.getElementById('save-the-date'), {
-      loop: false,
-      delay: 60
-    });
-
-    typewriter
-      .typeString('Annunciano con gioia il loro matrimonio')
-      // .pauseFor(400)
-      // .typeString('Sabato <strong>10</strong><br>')
-      // .pauseFor(400)
-      // .typeString('Ottobre <strong>2020</strong><br>')
-      // .pauseFor(300)
-      // .typeString('ore <strong>11.00</strong><br>')
-      // .pauseFor(400)
-      // .typeString('Chiesa dei Santi Filippo e Giacomo<br>')
-      // .pauseFor(200)
-      // .typeString('piazza Eremitani 9, Padova')
-      .start();
-  }
 });
