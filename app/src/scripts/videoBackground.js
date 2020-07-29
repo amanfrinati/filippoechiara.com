4e11c031656eec18479bb47b150ade250433d95a
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-07-30 00:21:12 +0200

    Remove comments. Remove unused js

--- app/src/scripts/videoBackground.js
+++ app/src/scripts/videoBackground.js
@@ -1,48 +1,16 @@
-import $ from "jquery";
+import $ from 'jquery'
 
-$(document).ready(function() {
-  const promise = document.querySelector('.party video').play();
+$(document).ready(function () {
+  const promise = document.querySelector('.party video').play()
 
   if (promise !== undefined) {
-    promise.catch(error => {
-      // Auto-play was prevented
-      // Show a UI element to let the user manually start playback
-    }).then(() => {
-      // Auto-play started
-    });
+    promise
+      .catch((error) => {
+        // Auto-play was prevented
+        // Show a UI element to let the user manually start playback
+      })
+      .then(() => {
+        // Auto-play started
+      })
   }
-
-  // const vid = document.getElementById("video-villa-gritti");
-
-  // const pauseButton = document.querySelector("#polina button");
-
-  // if (window.matchMedia('(prefers-reduced-motion)').matches) {
-  //   vid.removeAttribute("autoplay");
-  //   vid.pause();
-  //   pauseButton.innerHTML = "Paused";
-  // }
-
-  // function vidFade() {
-  //   vid.classList.add("stopfade");
-  // }
-
-  // vid.addEventListener('ended', function() {
-  //   // only functional if "loop" is removed
-  //   vid.pause();
-  //   // to capture IE10
-  //   vidFade();
-  // });
-
-  // pauseButton.addEventListener("click", function() {
-  //   vid.classList.toggle("stopfade");
-  //   if (vid.paused) {
-  //     vid.play();
-  //     pauseButton.innerHTML = "Pause";
-  //   } else {
-  //     vid.pause();
-  //     pauseButton.innerHTML = "Paused";
-  //   }
-  // })
-});
-
-
+})
