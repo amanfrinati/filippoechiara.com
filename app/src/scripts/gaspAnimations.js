1703ab4e4e9df41dacba6efbd3ec52c0a8fe8e5b
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-08-01 19:29:16 +0200

    Complete intro animation

--- app/src/scripts/gaspAnimations.js
+++ app/src/scripts/gaspAnimations.js
@@ -1,110 +1,125 @@
 import $ from 'jquery'
-import gasp from 'gsap'
+import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger.min'
+import ExpoScaleEase from 'gsap/EasePack'
 
-gasp.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ExpoScaleEase)
 
-const intro = gasp.timeline()
-intro
-  .to('.logo', {
-    width: 0,
-    duration: 1.5,
-    delay: 1
-    // ease: 'none'
-  })
-  .to('.logo-container', {
-    opacity: 0,
-    duration: 0.1
+const introAnimation = gsap.timeline()
+introAnimation
+  .to('.swiper-container.swiper-container-descriptions', {
+    opacity: 1,
+    duration: 0.5
   })
   .to('.img-intro-container', {
     opacity: 1,
-    duration: 0.1
+    duration: 0.5
   })
-  .to('.intro-together', {
-    css: {
-      scaleX: 1,
-      scaleY: 1
-    },
-    duration: 3
-    // ease: 'none'
+  .fromTo(
+    '.intro-together',
+    { scale: 50 },
+    { scale: 1, duration: 6, ease: 'expoScale(50, 1)' }
+  )
+  .to('.img-intro-container', {
+    rotateY: 90,
+    duration: 1.5,
+    delay: 0.5
   })
-// .to('.intro-together', {
-//   rotateY: 90,
-//   duration: 0.5
-//   // ease: 'none'
-// })
-intro.pause()
-
-$(document).ready(function () {
-  intro.play()
-
-  gasp.to('.sticky-container .sticky-content .madonna-corona', {
+  .to('.description-img.description-together', {
     opacity: 1,
-    duration: 1,
-    scrollTrigger: {
-      trigger: '.sticky-container .section-content',
-      start: 'bottom center',
-      end: '+=100%',
-      scrub: true
-    }
+    rotateY: 0,
+    duration: 1.5
   })
-
-  gasp.to('.eremitani-img', {
-    scale: 1,
-    duration: 1,
-    ease: 'expo.out',
-    scrollTrigger: {
-      trigger: '.hero-cerimonia',
-      start: 'top bottom+=10%',
-      end: 'bottom center',
-      scrub: true
-    }
-  })
-
-  gasp.to('.border-wrapper [class*="border-"]', {
-    scaleX: 0,
-    duration: 1,
-    ease: 'expo.out',
-    scrollTrigger: {
-      trigger: '.hero-cerimonia',
-      start: 'top bottom-=10%',
-      end: 'bottom center',
-      scrub: true
-    }
-  })
-
-  gasp.to('.video-bg', {
-    opacity: 0.5,
-    duration: 1,
-    scrollTrigger: {
-      trigger: '.video-text-container',
-      start: 'top bottom',
-      end: 'center center',
-      scrub: true
-    }
-  })
-
-  gasp.to('.final-section-hero', {
-    opacity: 0.5,
-    duration: 1,
-    ease: 'none',
-    scrollTrigger: {
-      trigger: '.final-section',
-      start: 'top+=10% center',
-      end: 'top+=50% center',
-      scrub: true
-    }
-  })
-
-  gasp.to('.final-section .phrase', {
+  .to('.swiper-container-descriptions .swiper-button, .swiper-pagination', {
     opacity: 1,
-    duration: 1,
-    ease: 'none',
-    scrollTrigger: {
-      trigger: '.final-section',
-      start: 'top+=10% center',
-      end: 'top+=50% center',
-      scrub: true
-    }
+    duration: 0.4
   })
+introAnimation.pause()
+
+$(window).scroll(() => {
+  if ($('.swiper-container.swiper-container-descriptions').visible(true)) {
+    introAnimation.play()
+  }
+})
+
+gsap.to('.logo-stick-content', {
+  scale: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.logo-landing .logo-stick-container',
+    start: 'top top',
+    end: 'bottom center',
+    scrub: true
+  }
+})
+
+gsap.to('.sticky-container .sticky-content .madonna-corona', {
+  opacity: 1,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.sticky-container .section-content',
+    start: 'bottom center',
+    end: '+=100%',
+    scrub: true
+  }
+})
+
+gsap.to('.eremitani-img', {
+  scale: 1,
+  duration: 1,
+  ease: 'expo.out',
+  scrollTrigger: {
+    trigger: '.hero-cerimonia',
+    start: 'top bottom+=10%',
+    end: 'bottom center',
+    scrub: true
+  }
+})
+
+gsap.to('.border-wrapper [class*="border-"]', {
+  scaleX: 0,
+  duration: 1,
+  ease: 'expo.out',
+  scrollTrigger: {
+    trigger: '.hero-cerimonia',
+    start: 'top bottom-=10%',
+    end: 'bottom center',
+    scrub: true
+  }
+})
+
+gsap.to('.video-bg', {
+  opacity: 0.5,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.video-text-container',
+    start: 'top bottom',
+    end: 'center center',
+    scrub: true
+  }
+})
+
+gsap.to('.final-section-hero', {
+  opacity: 0.5,
+  duration: 1,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.final-section',
+    start: 'top+=10% center',
+    end: 'top+=50% center',
+    scrub: true
+  }
+})
+
+gsap.to('.final-section .phrase', {
+  opacity: 1,
+  duration: 1,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.final-section',
+    start: 'top+=10% center',
+    end: 'top+=50% center',
+    scrub: true
+  }
 })
